cd473aafc73a691993465b995bd44fdb877ae51e
Rolzad73 <rolzad73@gmail.com>
2015-07-04 14:49:38 -0400

    - duplicated functionality in tv module

---
+++ tv/src/main/java/com/roltekk/daydream/gameoflife/DevelopmentActivity.java
@@ -0,0 +1,44 @@
+package com.roltekk.daydream.gameoflife;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.roltekk.daydream.gameoflife.core.DishView;
+
+public class DevelopmentActivity extends Activity {
+    private static final String TAG = "DevelopmentActivity";
+    private DishView mDishView;
+    private int mTestDishWidth = 32; // 1920 / 60
+    private int mTestDishHeight  = 18; // 1080 / 60
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate");
+        super.onCreate(savedInstanceState);
+        mDishView = new DishView(this, mTestDishWidth, mTestDishHeight);
+        setContentView(mDishView);
+        mDishView.StartViewThread();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume");
+        super.onResume();
+        mDishView.Resume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause");
+        super.onPause();
+        mDishView.Pause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy");
+        super.onDestroy();
+        mDishView.requestStop();
+    }
+}
